681366b57ce0faf75fe091e71472843044fa19cf
JohanAcaro <93380488+JohanAcaro@users.noreply.github.com>
2021-12-10 12:59:02 +0100

    Update and rename Ejercicio1.java to Ejercicio4.java

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,12 +1,12 @@
 package com.ejercicios;
 
-public class Ejercicio1 {
+public class Ejercicio4 {
 
 	public static void main(String[] args) {
 		String sCadenaInvertida;
 		sCadenaInvertida="";
 
-		String sCadena = "Texto al revés";
+		String sCadena = "Texto al revÃ©s";
 		for (int x=sCadena.length()-1;x>=0;x--)
 			  sCadenaInvertida = sCadenaInvertida + sCadena.charAt(x);
 		System.out.println(sCadenaInvertida);
